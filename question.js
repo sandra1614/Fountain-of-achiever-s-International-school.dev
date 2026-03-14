9ecc640e5a2e3aeb2971027328333e9d9572abcb
Lateef Amoo OLawale <lateefamoo57@gmail.com>
2026-03-14 18:14:12 +0100

    Update question.js

--- question.js
+++ question.js
@@ -22,24 +22,17 @@ solution: "10 - 4 = 6"
 },
 
 {
-question: "What is the name of the Fountain of Achiever’s school proprietor?",
-options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
-correct: 0,
-solution: "The proprietor is Mr Akinfolarin."
-},
-
-{
 question: "What is the product of 15 and 2?",
-options: ["25","152","30"],
+options: ["25","152","30","20"],
 correct: 2,
 solution: "15 × 2 = 30"
 },
 
 {
-question: "What is the name of the Fountain of Achiever’s school proprietress?",
-options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
-correct: 2,
-solution: "The proprietress is Mrs Akinfolarin."
+question: "Which planet do we live on?",
+options: ["Mars","Earth","Jupiter","Venus"],
+correct: 1,
+solution: "Humans live on Earth"
 }
 
 ];
