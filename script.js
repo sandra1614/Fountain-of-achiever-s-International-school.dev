fc24e2f3274dc790348966e21a287cdacb2e85ed
Lateef Amoo OLawale <lateefamoo57@gmail.com>
2026-03-14 18:04:08 +0100

    Add quiz functionality with start and submit options

---
+++ script.js
@@ -0,0 +1,86 @@
+function startQuiz(){
+
+let name = document.getElementById("studentName").value;
+
+if(name === ""){
+alert("Please enter your name");
+return;
+}
+
+document.getElementById("startPage").style.display="none";
+document.getElementById("quizPage").style.display="block";
+
+document.getElementById("studentDisplay").innerHTML="Student: "+name;
+
+loadQuiz();
+
+}
+
+
+const quizContainer = document.getElementById("quiz");
+
+
+function loadQuiz(){
+
+quizQuestions.forEach((q,index)=>{
+
+let html = `<div class="question">
+<p>${index+1}. ${q.question}</p>`;
+
+q.options.forEach((opt,i)=>{
+
+html += `
+<label>
+<input type="radio" name="q${index}" value="${i}">
+${opt}
+<span id="mark${index}${i}"></span>
+</label><br>
+`;
+
+});
+
+html += `<p id="sol${index}" class="solution"></p>`;
+html += `</div>`;
+
+quizContainer.innerHTML += html;
+
+});
+
+}
+
+
+function submitQuiz(){
+
+let score = 0;
+
+quizQuestions.forEach((q,index)=>{
+
+let selected = document.querySelector(`input[name="q${index}"]:checked`);
+
+if(selected){
+
+let answer = parseInt(selected.value);
+
+if(answer === q.correct){
+
+score++;
+document.getElementById(`mark${index}${answer}`).innerHTML=" ✔";
+
+}else{
+
+document.getElementById(`mark${index}${answer}`).innerHTML=" ✖";
+document.getElementById(`mark${index}${q.correct}`).innerHTML=" ✔";
+
+}
+
+}
+
+document.getElementById(`sol${index}`).innerHTML =
+"Solution: " + q.solution;
+
+});
+
+document.getElementById("score").innerHTML =
+"Your Score: " + score + " / " + quizQuestions.length;
+
+}
